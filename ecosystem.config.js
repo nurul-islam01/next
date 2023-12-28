ceb272b98739f2faf7083f83c0746502e3714a2a
nurul-islam-prothomalo <nurul.islam@prothomalo.com>
2023-12-28 09:06:25 +0600

    updateing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,21 @@
 module.exports = {
   apps: [
     {
-      name: 'nurul',
-      script: 'npm start',
-      port: 3000
+      script: 'npm start'
     }
-  ]
+  ],
+  deploy: {
+    production: {
+      user: 'nurul',
+      host: '62.72.24.228',
+      ref: 'origin/main',
+      repo: 'git@github.com:nurul-islam01/next.git',
+      path: '/home/nurul/htdocs/nurulislam.dev',
+      'pre-deploy-local': '',
+      'post-deploy':
+        'source ~/.nvm/nvm.sh && npm i && npm run build && pm2 reload ecosystem.config.js -env production',
+      'pre-setup': '',
+      ssh_options: 'ForwardAgent=yes'
+    }
+  }
 };
